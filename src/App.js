50e68bfce6d2e2a213b2afcb1c3311d975ea333a
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-03-06 14:23:04 -0500

    GET Request

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from "react"
 import Button from 'react-bootstrap/Button';
 import Parks from './Parks';
 import Header from './Header';
 
 
 function App() {
+  const [parks, setParks] = useState([])
+
+  useEffect(() => {
+    fetch("")
+      .then(r => r.json())
+      .then(setParks)
+  }, [])
+
   return (
     <div>
       <Header></Header>
-      <Parks />
+      <Parks parks={parks}/>
     </div>
   );
 }
