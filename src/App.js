1bc004ce8f1b14bd3d90d7b15bc75b199f4a1a53
Kyle <kyleoneill26@gmail.com>
2023-03-06 11:58:36 -0600

    fix header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import Parks from './Parks';
+import Header from './Header';
 
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Parks />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 function Header () {
-    
+
   return (
     <header>
-      <img src="" alt="Banner" />
+      <img style = {{width: 1400, height: 300}}src="https://cdn.cheapism.com/images/National_Park_Photos.2e16d0ba.fill-1440x605.png" alt="Banner" />
       <div>
         <button>About Page</button>
         <button>Home </button>
