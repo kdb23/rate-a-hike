d5d9074fafcaa8a7b5421e38906d3c8eef82f37f
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-03-07 16:05:27 -0500

    popover addition

--- src/ParkCard.js
+++ src/ParkCard.js
@@ -1,23 +1,38 @@
 import React from 'react'
-import {Container, Row, Col, Button } from 'react-bootstrap'
+import {Container, Row } from 'react-bootstrap'
+import { Popover,OverlayTrigger, ButtonToolbar, Button } from 'react-bootstrap';
 
 
-function ParkCard({image,name}) {
+function ParkCard({image,name, peak, location, size, description}) {
+  const popoverClick = (
+    <Popover id="popover-trigger-click" title="Testing PopOver">
+      <h4>{name}</h4>
+      <p>Description:{description}</p>
+      <p>Location:{location}</p>
+      <p>Size:{size}</p>
+      <p>Highest Peak:{peak}</p>
+      </Popover>
+  );
 
   return (
     <Container>
       <Row>
         <div class="col-4">
-    <div class="card">
+       <div class="card">
         <img class="card-img-top" src={image} alt={name} />
         <div class="card-body">
             <p class="card-text">{name}</p>
         </div>
-        <Button type="button" class="btn btn-primary">View More</Button>
+      <ButtonToolbar>
+      <OverlayTrigger trigger="click" placement="right" overlay={popoverClick}>
+      <Button>View More</Button>
+    </OverlayTrigger>
+    </ButtonToolbar>
     </div>
     </div>
     </Row>
    </Container> 
+  
   )
 }
 
--- src/Parks.js
+++ src/Parks.js
@@ -7,6 +7,10 @@ function Parks({parks}) {
         key={parkObj.id}
         name={parkObj.name}
         image={parkObj.image}
+        description={parkObj.description}
+        location={parkObj.location}
+        size={parkObj.size}
+        peak={parkObj["highest-point"]}
       />
   }))
   return (
