3ee2f0ca1ae3e9b6e191bbec3da820ce6201d656
Kyle <kyleoneill26@gmail.com>
2023-03-07 10:02:59 -0600

    starting to set up home switch

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 
 import './App.css';
-import {useEffect, useState} from "react"
-import Parks from './Parks';
+//import {useEffect, useState} from "react"
+//import Parks from './Parks';
 import Header from './Header';
 import ParkForm from './ParkForm';
 import NavBar from './NavBar';
 import { Carousel } from 'react-bootstrap';
-
+import { Route } from 'react-router-dom';
+import Home from './Home'
 
 function App() {
-  const [parks, setParks] = useState([])
+  // const [parks, setParks] = useState([])
 
-  useEffect(() => {
-    fetch("http://localhost:3002/parks")
-      .then(r => r.json())
-      .then(setParks)
-  }, [])
+  // useEffect(() => {
+  //   fetch("http://localhost:3002/parks")
+  //     .then(r => r.json())
+  //     .then(setParks)
+  // }, [])
 
-  const addProject = (newPark) => {
-    setParks([...parks, newPark])
-  }
+  // const addProject = (newPark) => {
+  //   setParks([...parks, newPark])
+  //}
 
   return (
     <div>
-      <Header></Header>
+      <Route exact path = "/"> 
+      <Home />
+      </Route>
+      {/* <Route exact path = "/NavBar"> 
+      <NavBar />
+      </Route> */}
+       <Header></Header>
       <Carousel></Carousel>
-      <NavBar></NavBar>
+      <Route exact path = "/NavBar"> 
+      <NavBar />
+      </Route>
+      {/* <NavBar></NavBar>
       <ParkForm addProject={addProject}/>
-      <Parks parks={parks}/>
+      <Parks parks={parks}/>  */}
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,36 @@
+//import './App.css';
+import {useEffect, useState} from "react"
+import Parks from './Parks';
+import Header from './Header';
+import ParkForm from './ParkForm';
+import NavBar from './NavBar';
+import { Carousel } from 'react-bootstrap';
+//import { Route } from 'react-router-dom';
+
+function Home () {
+const [parks, setParks] = useState([])
+
+useEffect(() => {
+    fetch("http://localhost:3002/parks")
+      .then(r => r.json())
+      .then(setParks)
+  }, [])
+
+  const addProject = (newPark) => {
+    setParks([...parks, newPark])
+  }
+
+  
+    return (
+        <div>
+        <Header></Header>
+        <Carousel></Carousel>
+        <NavBar></NavBar>
+        <ParkForm addProject={addProject}/>
+        <Parks parks={parks}/>
+      </div>  
+    )
+
+  }
+
+  export default Home;
