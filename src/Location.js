ae5c76ce1a503fee359e8ef8f281b6f281d1505a
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-03-07 21:53:51 -0500

    addition of code minus API key for location route

--- src/Location.js
+++ src/Location.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker  } from 'google-maps-react';
 
-function Location() {
-  return (
-    <div>Find the Closest National Park to You !</div>
-  )
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+class Demo1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Map
+          google={this.props.google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={{
+            lat: YOUR_LATITUDE,
+            lng: YOUR_LONGITUDE
+          }}
+        >
+         <Marker
+          onClick={this.onMarkerClick}
+          name={'This is test name'}
+        />
+        </Map>
+      </div>
+    );
+  }
 }
 
-export default Location
+export default GoogleApiWrapper({
+  apiKey: 'API_KEY'
+})(Demo1);
